55f6df352348550ddedd9607f4fccdf284779ac2
yazloysasha <yazloysasha@gmail.com>
2026-01-17 16:11:36 +0700

    fix: AnnotateClimateMixin

--- src/main/java/net/yazloysasha/tfcrealworld/mixin/world/region/AnnotateClimateMixin.java
+++ src/main/java/net/yazloysasha/tfcrealworld/mixin/world/region/AnnotateClimateMixin.java
@@ -1,21 +1,15 @@
 package net.yazloysasha.tfcrealworld.mixin.world.region;
 
-import java.util.Iterator;
 import net.dries007.tfc.world.noise.Noise2D;
 import net.dries007.tfc.world.region.AnnotateClimate;
-import net.dries007.tfc.world.region.Region;
-import net.dries007.tfc.world.region.RegionGenerator;
 import net.dries007.tfc.world.region.Units;
 import net.minecraft.util.Mth;
 import net.yazloysasha.tfcrealworld.config.TFCRealWorldConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(AnnotateClimate.class)
+@Mixin(value = AnnotateClimate.class, remap = false)
 public class AnnotateClimateMixin {
 
   @Redirect(
@@ -46,35 +40,6 @@ public class AnnotateClimateMixin {
   }
 
   /**
-   * Overrides rainfallVariance calculation when using map.
-   */
-  @Inject(
-    method = "apply",
-    at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/util/Mth;lerp(FFF)F",
-      ordinal = 0
-    ),
-    locals = LocalCapture.CAPTURE_FAILHARD,
-    cancellable = false
-  )
-  private void tfcrealworld$overrideRainfallVariance(
-    RegionGenerator.Context context,
-    CallbackInfo ci,
-    Iterator<?> iterator,
-    Region.Point point,
-    int x,
-    int z,
-    float bias
-  ) {
-    if (TFCRealWorldConfig.KOPPEN_FROM_MAP.get()) {
-      point.rainfallVariance = (float) context
-        .generator()
-        .rainfallVarianceNoise.noise(x, z);
-    }
-  }
-
-  /**
    * Disables temperature modification based on bias and ocean proximity when using Köppen map.
    */
   @Redirect(
@@ -82,29 +47,7 @@ public class AnnotateClimateMixin {
     at = @At(
       value = "INVOKE",
       target = "Lnet/minecraft/util/Mth;lerp(FFF)F",
-      ordinal = 1
-    )
-  )
-  private float tfcrealworld$preserveTemperatureBiasTarget(
-    float delta,
-    float start,
-    float end
-  ) {
-    if (TFCRealWorldConfig.KOPPEN_FROM_MAP.get()) {
-      return end;
-    }
-    return Mth.lerp(delta, start, end);
-  }
-
-  /**
-   * Disables temperature modification based on tempDelta and oceanic influence when using Köppen map.
-   */
-  @Redirect(
-    method = "apply",
-    at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/util/Mth;lerp(FFF)F",
-      ordinal = 2
+      ordinal = 0
     )
   )
   private float tfcrealworld$preserveTemperatureFromMap(
@@ -126,32 +69,10 @@ public class AnnotateClimateMixin {
     at = @At(
       value = "INVOKE",
       target = "Lnet/minecraft/util/Mth;lerp(FFF)F",
-      ordinal = 3
+      ordinal = 1
     )
   )
   private float tfcrealworld$preserveRainfallFromMap(
-    float delta,
-    float start,
-    float end
-  ) {
-    if (TFCRealWorldConfig.KOPPEN_FROM_MAP.get()) {
-      return start;
-    }
-    return Mth.lerp(delta, start, end);
-  }
-
-  /**
-   * Disables rainfallVariance reduction at cell edges when using rainVar map.
-   */
-  @Redirect(
-    method = "apply",
-    at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/util/Mth;lerp(FFF)F",
-      ordinal = 4
-    )
-  )
-  private float tfcrealworld$preserveRainfallVarianceFromMap(
     float delta,
     float start,
     float end
