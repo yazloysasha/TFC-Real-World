09e677a89b91d4a52c97115d923a2010e41ede1a
yazloysasha <yazloysasha@gmail.com>
2026-01-20 13:32:57 +0700

    fix: spawn distance

--- src/main/java/net/yazloysasha/tfcrealworld/config/TFCRealWorldConfig.java
+++ src/main/java/net/yazloysasha/tfcrealworld/config/TFCRealWorldConfig.java
@@ -109,8 +109,8 @@ public class TFCRealWorldConfig {
       BUILDER,
       "spawn_distance",
       "Spawn distance in blocks",
-      100,
-      100,
+      256,
+      256,
       20_000
     );
 
